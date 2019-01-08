09b50173ee76f7e222d6b0054fadf64564c692eb
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-07 20:46:56 -0700

    add sample meals

---
+++ src/meals.js
@@ -0,0 +1,8 @@
+const { randomInt, randomArrayElement } = require('./utils')
+
+const meals = {
+  breakfast: [':pineapple', ':banana', ':eggs', ':bread:'],
+  lunch: [':taco:', ':sandwich:', ':burrito', ':hamburger:'],
+  supper: [':pizza:', ':spaghetti', ':takeout_box:', ':ramen:'],
+  dessert: [':icecream:', ':shaved_ice:', ':cake:', ':melon:']
+}
