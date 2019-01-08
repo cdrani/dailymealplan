9c69801b0702e0616cf7b5a27f9201a83aa34d69
cdrainxv <cdrainxv@users.noreply.github.com>
2019-01-08 00:04:11 -0700

    addToMealPlan implementation

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,12 @@
 const Twit = require('twit')
 const config = require('./config')
+const { addToMealPlan } = require('./meals')
 
-const bot = new Twit(config)
+const search = () => {
+  const bot = new Twit(config)
+  bot.get('search/tweets', { q: 'banana', count: 5 }, (err, data, res) => {
+    console.log(addToMealPlan())
+  })
+}
+
+module.exports = { search }
--- src/meals.js
+++ src/meals.js
@@ -1,8 +1,40 @@
-const { randomInt, randomArrayElement } = require('./utils')
+const { randomArrayElement } = require('./utils')
 
 const meals = {
-  breakfast: [':pineapple', ':banana', ':eggs', ':bread:'],
-  lunch: [':taco:', ':sandwich:', ':burrito', ':hamburger:'],
-  supper: [':pizza:', ':spaghetti', ':takeout_box:', ':ramen:'],
-  dessert: [':icecream:', ':shaved_ice:', ':cake:', ':melon:']
+  breakfast: ['🍳', '🍎', '🍌', '🍞', '🥞', '🥓'],
+  lunch: ['🍕', '🌯', '🌮', '🥙', '🥗', '🥔', '🥓', '🍔'],
+  supper: ['🍜', '🍖', '🍲', '🍗', '🍚', '🍙', '🥓', '🥗'],
+  dessert: ['🍦', '🍧', '🍰', '🍡', '🍠', '🍘', '🥓', '🍮'],
+  beverage: ['🥛', '🚰', '🍷', '🍸', '🍺', '🍻', '🥃', '🍵', '🥓']
 }
+
+const selectMeals = mealRations => {
+  const mealPlan = {
+    breakfast: [],
+    lunch: [],
+    supper: [],
+    dessert: []
+  }
+
+  Object.keys(mealRations).forEach(option => {
+    for (let i = 0; i < mealRations[option]; i++) {
+      mealPlan[option].push(randomArrayElement(meals[option]))
+    }
+    mealPlan[option].push(randomArrayElement(meals.beverage))
+  })
+
+  return mealPlan
+}
+
+const addToMealPlan = () => {
+  const mealRations = {
+    breakfast: 3,
+    lunch: 2,
+    supper: 3,
+    dessert: 2,
+  }
+
+  return selectMeals(mealRations)
+}
+
+module.exports = { addToMealPlan }
